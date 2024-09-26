f23910b688778923a6a7c183689facebfcd2d7d4
dinar <saetonskie@mail.ru>
2024-09-26 17:35:14 +0300

    complited

--- menu/src/components/Menu.jsx
+++ menu/src/components/Menu.jsx
@@ -1,22 +1,17 @@
-import {Link} from 'react-router-dom'
+import { NavLink} from 'react-router-dom'
 
 
 export default function Menu() {
 
-  const clickHandler = (event)=>{
-    event.currentTarget.childNodes.forEach((item)=> {
-      item.classList.remove('menu__item-active')
-    })
-    event.target.classList.add('menu__item-active');
-  }
+
     return (
-    <nav onClick={clickHandler} class="menu">
-      <Link  className="menu__item" to="/"
+    <nav  class="menu">
+      <NavLink  className={({isActive}) => isActive ? "menu__item menu__item-active" : "menu__item"} to="/"
       >  Главная
-      </Link>
-      <Link className="menu__item" to="/drift">Дрифт-такси</Link>
-      <Link className="menu__item" to="/timeattack">Time Attack</Link>
-      <Link className="menu__item" to="/forza">Forza Karting</Link>
+      </NavLink>
+      <NavLink className={({isActive}) => isActive ? "menu__item menu__item-active" : "menu__item"} to="/drift">Дрифт-такси</NavLink>
+      <NavLink className={({isActive}) => isActive ? "menu__item menu__item-active" : "menu__item"} to="/timeattack">Time Attack</NavLink>
+      <NavLink className={({isActive}) => isActive ? "menu__item menu__item-active" : "menu__item"} to="/forza">Forza Karting</NavLink>
   </nav>
   );
   }
